647f344613c3ffef506a660cf220822d83b61518
Ev-Hoang <hoangduckien0402@gmail.com>
2025-08-28 00:09:37 +0700

    Update

--- app/workers/ai_worker.py
+++ app/workers/ai_worker.py
@@ -8,12 +8,16 @@ async def ai_worker():
     Worker dự đoán hành động và gửi command qua UART
     """
     print("AI worker started")
+    previous_label = "nocommand"
     while True:
         feat = await queue.get()
         try:
             label, prob = ai_model.predict_action(feat)
             if uart.is_serial_connected() and label is not None:
-                asyncio.create_task(uart.send_command_async(label))
+                if(previous_label != label and prob > 0.8 ):
+                    previous_label = label
+                    if(label != "nocommand"):
+                        asyncio.create_task(uart.send_command_async(label))
         except Exception as e:
             print("Predict error:", e)
         finally:
